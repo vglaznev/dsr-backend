cc1537a75987325cfdf9691c644a5b8b71d2665a
Vsevolod Glaznev <vsevolod.glaznev@gmail.com>
2022-05-10 22:58:16 +0300

    fix comments in RandomStringGenerator

--- src/main/java/com/vglaznev/shorturlservice/generator/RandomStringGenerator.java
+++ src/main/java/com/vglaznev/shorturlservice/generator/RandomStringGenerator.java
@@ -3,19 +3,18 @@ package com.vglaznev.shorturlservice.generator;
 import java.security.SecureRandom;
 import java.util.Base64;
 
-//TODO: rename class (choose correct name)
 public class RandomStringGenerator {
     private static final SecureRandom random = new SecureRandom();
     private static final Base64.Encoder encoder = Base64.getUrlEncoder().withoutPadding();
     private static final int ID_DEFAULT_LENGTH = 8;
 
     /**
-     * Generate a unique string of length 8 in base 64 alphabet.
-     * Amount of unique strings that method can produce is 64^8 (approx. 2e+14).
+     * Generate a random string of length 8 in base 64 alphabet.
+     * Amount of random strings that method can produce is 64^8 (approx. 2e+14).
      * However, method use random function to generate strings, so collisions can occur.
-     * (before all 2e+14 unique strings are used)
+     * (before all 2e+14 random strings are used)
      *
-     * @return unique string
+     * @return random string
      */
     public static String generate() {
         //base64 use 6 bit for character and byte is 8 bit
