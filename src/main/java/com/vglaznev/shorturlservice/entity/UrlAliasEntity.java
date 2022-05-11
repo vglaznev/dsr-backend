0e3cb4aaa8c33c2c7d7a908c23f8f588f3e7387a
Vsevolod Glaznev <vsevolod.glaznev@gmail.com>
2022-05-12 00:45:21 +0300

    replace lombok equals and hash

--- src/main/java/com/vglaznev/shorturlservice/entity/UrlAliasEntity.java
+++ src/main/java/com/vglaznev/shorturlservice/entity/UrlAliasEntity.java
@@ -1,23 +1,47 @@
 package com.vglaznev.shorturlservice.entity;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Table(name = "url_table")
-@Data
+@Getter
+@Setter
+@ToString //for debug
 @NoArgsConstructor
 @AllArgsConstructor
 public class UrlAliasEntity {
     @Id
     private String shortUrlId;
+
     @Column(nullable = false)
     private String originalUrl;
+
+    @Override
+    public int hashCode() {
+        return getShortUrlId().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        UrlAliasEntity other = (UrlAliasEntity) obj;
+        return Objects.equals(shortUrlId, other.getShortUrlId());
+    }
+
+
 }
